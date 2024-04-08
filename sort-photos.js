ffa8b1cce17d20a0707cbe2d51cfd7a53f95ce21
ornel77 <ornella.tomboza@gmail.com>
2024-04-08 13:09:19 +0200

    script for delete and sorting files ok

---
+++ sort-photos.js
@@ -0,0 +1,62 @@
+const fs = require("fs");
+const path = require("path");
+
+// fs.rename('/Users/ornella/Documents/camera-raw-jpeg/raw', '/Users/ornella/Documents/camera-raw-jpeg/raw-new', (err) => {
+//     if(err) throw err
+//     console.log('rename completed')
+// })
+
+const textExtension = [".txt", ".texte"];
+const wordExtension = [".doc", ".docx"];
+
+function sortFiles(sourceDir, textDir, wordDir) {
+  // make sure directory exist, otherwise create it
+  if (!fs.existsSync(textDir)) {
+    fs.mkdirSync(textDir, { recursive: true });
+    console.log(`${textDir} created`);
+  }
+  if (!fs.existsSync(textDir)) {
+    fs.mkdirSync(textDir, { recursive: true });
+    console.log(`${textDir} created`);
+  }
+
+  const wordDirFiles = fs.readdirSync(wordDir);
+  const textDirFiles = fs.readdirSync(textDir);
+
+  // Read files in the source directory
+  fs.readdirSync(sourceDir).forEach((filename) => {
+    const sourceFilePath = path.join(sourceDir, filename);
+    const ext = path.extname(filename);
+    if (textExtension.includes(ext)) {
+        if(!textDirFiles.includes(filename)) {
+
+            fs.renameSync(sourceFilePath, path.join(textDir, filename));
+            console.log(`Moved ${filename} to ${textDir}`);
+        } else {
+        console.log(`${filename} already exits in ${textDir}`)
+
+        }
+    } else if (wordExtension.includes(ext)) {
+      if (!wordDirFiles.includes(filename)) {
+        fs.renameSync(sourceFilePath, path.join(wordDir, filename));
+        console.log(`Moved ${filename} to ${wordDir}`);
+      } else {
+        console.log(`${filename} already exits in ${wordDir}`)
+      }
+    }
+  });
+}
+
+function deleteFile(sourceDir) {
+    fs.readdirSync(sourceDir).forEach(filename => {
+        fs.unlinkSync(path.join(sourceDir, filename))
+    })
+}
+
+deleteFile('/Users/ornella/Documents/camera-raw-jpeg/text')
+
+// sortFiles(
+//   "/Users/ornella/Documents/camera-raw-jpeg",
+//   "/Users/ornella/Documents/camera-raw-jpeg/text",
+//   "/Users/ornella/Documents/camera-raw-jpeg/word"
+// );
