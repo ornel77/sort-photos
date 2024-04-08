ed119771b6a258d72a0749430902ddc699ae1faf
ornel77 <ornella.tomboza@gmail.com>
2024-04-08 13:50:48 +0200

    rename variables

--- sort-photos.js
+++ sort-photos.js
@@ -6,56 +6,50 @@ const path = require("path");
 //     console.log('rename completed')
 // })
 
-const textExtension = [".txt", ".texte"];
-const wordExtension = [".doc", ".docx"];
+const rawExtension = [".cr2", ".cr3"];
+const jpegExtension = [".jpeg", ".jpg"];
 
-function sortFiles(sourceDir, textDir, wordDir) {
+function sortFiles(sourceDir, rawDir, jpegDir) {
   // make sure directory exist, otherwise create it
-  if (!fs.existsSync(textDir)) {
-    fs.mkdirSync(textDir, { recursive: true });
-    console.log(`${textDir} created`);
+  if (!fs.existsSync(rawDir)) {
+    fs.mkdirSync(rawDir, { recursive: true });
+    console.log(`${rawDir} created`);
   }
-  if (!fs.existsSync(textDir)) {
-    fs.mkdirSync(textDir, { recursive: true });
-    console.log(`${textDir} created`);
+  if (!fs.existsSync(rawDir)) {
+    fs.mkdirSync(rawDir, { recursive: true });
+    console.log(`${rawDir} created`);
   }
 
-  const wordDirFiles = fs.readdirSync(wordDir);
-  const textDirFiles = fs.readdirSync(textDir);
+  const jpegDirFiles = fs.readdirSync(jpegDir);
+  const rawDirFiles = fs.readdirSync(rawDir);
 
   // Read files in the source directory
   fs.readdirSync(sourceDir).forEach((filename) => {
     const sourceFilePath = path.join(sourceDir, filename);
     const ext = path.extname(filename);
-    if (textExtension.includes(ext)) {
-        if(!textDirFiles.includes(filename)) {
-
-            fs.renameSync(sourceFilePath, path.join(textDir, filename));
-            console.log(`Moved ${filename} to ${textDir}`);
-        } else {
-        console.log(`${filename} already exits in ${textDir}`)
-
-        }
-    } else if (wordExtension.includes(ext)) {
-      if (!wordDirFiles.includes(filename)) {
-        fs.renameSync(sourceFilePath, path.join(wordDir, filename));
-        console.log(`Moved ${filename} to ${wordDir}`);
+    if (rawExtension.includes(ext)) {
+      // check if the file is already in the folder
+      if (!rawDirFiles.includes(filename)) {
+        fs.renameSync(sourceFilePath, path.join(rawDir, filename));
+        console.log(`Moved ${filename} to ${rawDir}`);
       } else {
-        console.log(`${filename} already exits in ${wordDir}`)
+        console.log(`${filename} already exits in ${rawDir}`);
+      }
+    } else if (jpegExtension.includes(ext)) {
+      if (!jpegDirFiles.includes(filename)) {
+        fs.renameSync(sourceFilePath, path.join(jpegDir, filename));
+        console.log(`Moved ${filename} to ${jpegDir}`);
+      } else {
+        console.log(`${filename} already exits in ${jpegDir}`);
       }
     }
   });
 }
 
 function deleteFile(sourceDir) {
-    fs.readdirSync(sourceDir).forEach(filename => {
-        fs.unlinkSync(path.join(sourceDir, filename))
-    })
+  fs.readdirSync(sourceDir).forEach((filename) => {
+    fs.unlinkSync(path.join(sourceDir, filename));
+  });
 }
 
-
-sortFiles(
-  "",
-  "",
-  ""
-);
+sortFiles("", "", "");
